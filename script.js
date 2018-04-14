2364fa782e8b03d06d624d1de56e16eac60474d6
Anthony Lapham <lapham.2@wright.edu>
2018-04-14 09:29:05 -0400

    style hover

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
   $('#user').hover(function(){
-    $(this).html($("<span>User Image</span>"));
+    $(this).html($("<span>User Image</span>").css("display", "block", "font-size", "11px"));
   },
   function(){
     $(this).html($(""));
   }
 );
 $('#mic').hover(function(){
-  $(this).html($("<span>Search by voice</span>")).css();
+  $(this).html($("<span id=>Search by voice</span>").css("display", "block"));
 },
 function(){
   $(this).html($(""));
 }
 );
 $("#notification").hover(function(){
-  $(this).html($("<span>Google Notifications</span>"));
+  $(this).html($("<span>Google Notifications</span>").css("display", "block"));
 },
 function(){
   $(this).html($(""));
