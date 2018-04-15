6c6f8f74f1f36b41b90a8dda2c3f71df27e0d8f9
Anthony Lapham <lapham.2@wright.edu>
2018-04-15 08:12:42 -0400

    styling

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-  $('#user').hover(function(){
-    $(this).html($("<span>User Image</span>").css("display", "block", "font-size", "11px"));
-  },
-  function(){
-    $(this).html($(""));
-  }
-);
-$('#mic').hover(function(){
-  $(this).html($("<span id=>Search by voice</span>").css("display", "block"));
-},
-function(){
-  $(this).html($(""));
-}
-);
-$("#notification").hover(function(){
-  $(this).html($("<span>Google Notifications</span>").css("display", "block"));
-},
-function(){
-  $(this).html($(""));
-}
-);
+  $('#user').hover(function() {
+      $(this).html($("<span>User Image</span>").css("display", "block").css("font-size", "12px"));
+    },
+    function() {
+      $(this).html($(""));
+    }
+  );
+  $('#mic').hover(function() {
+      $(this).html($("<span id=>Search by voice</span>").css("display", "block").css("color", "white").css("font-size", "12px").css("position", "absolute").css("top", "25px").css("padding", "5px").css("border-radius", "10px").css("background-color", "black"));
+    },
+    function() {
+      $(this).html($(""));
+    }
+  );
+  $("#notification").hover(function() {
+      $(this).html($("<span>Google Notifications</span>").css("display", "block").css("font-size", "12px"));
+    },
+    function() {
+      $(this).html($(""));
+    }
+  );
 });
